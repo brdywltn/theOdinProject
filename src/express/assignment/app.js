bff04a607c0830b0b0f9a856bbbf57a1339732fb
brdywltn <bwilton@companieshouse.gov.uk>
2024-12-12 15:19:30 +0000

    implement new message functionality and view individual messages on their own page

--- src/express/assignment/app.js
+++ src/express/assignment/app.js
@@ -5,6 +5,8 @@ const path = require('node:path');
 const assetsPath = path.join(__dirname, 'public');
 app.use(express.static(assetsPath));
 
+app.use(express.urlencoded({ extended: true }));
+
 const messages = [
     {
         text: "Hi there!",
@@ -20,7 +22,28 @@ const messages = [
 
 app.get("/", (req, res) => {
     res.render("index", { messages: messages });
-})
+});
+
+app.get("/new", (req, res) => {
+   res.render("form");
+});
+
+app.get("/open", (req, res) => {
+   res.render("message", {
+       text: req.query.text,
+       user: req.query.user,
+       added: req.query.added,
+   });
+});
+
+app.post("/new", (req, res) => {
+   let name = req.body.name;
+   let message = req.body.message;
+   let added = new Date();
+
+   messages.push({text: message, user: name, added: added});
+   res.redirect("/");
+});
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
