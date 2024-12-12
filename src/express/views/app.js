6eabc9b8de0701943b8b983f960015e52d8fa54b
brdywltn <bwilton@companieshouse.gov.uk>
2024-12-12 13:18:40 +0000

    Add in final ejs views stuff

--- src/express/views/app.js
+++ src/express/views/app.js
@@ -5,14 +5,28 @@ const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
 
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
 const links = [
     { href: "/", text: "Home" },
     { href: "about", text: "About"},
 ];
 
+const users = [
+    "Rose",
+    "Cake",
+    "Biff",
+    "Chip",
+];
+
 app.get("/", (req, res) => {
-    res.render("index", { links: links });
-})
+    res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { links: links });
+});
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
